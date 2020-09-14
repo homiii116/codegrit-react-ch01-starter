dd615373f0c7c2be330774491bdd5633f0fcdf80
miho yamagata <miho.yamagata@gmail.com>
2020-09-14 21:46:19 +0900

    edited App AppTwo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { createElement, Component } from 'react'
 
 class App extends Component {
+
   render() {
     const h1 = React.createElement('h1', null, 'これは一つ目のアプリです');
     ReactDOM.render(h1, document.getElementById('root'));
+    return h1;
   }
 }
 
--- src/AppTwo.js
+++ src/AppTwo.js
@@ -1,9 +1,11 @@
 import { createElement, Component } from 'react'
 
 class AppTwo extends Component {
+
   render() {
     const h1 = React.createElement('h1', null, 'これは二つ目のアプリです');
     ReactDOM.render(h1, document.getElementById('root-two'));
+    return h1;
   }
 }
 
